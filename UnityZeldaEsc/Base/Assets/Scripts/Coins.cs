d07ded5b0a9517180d337ba3c045b081a2ded082
Syk-i <34940095+Syk-i@users.noreply.github.com>
2018-12-31 11:28:35 +0300

    Add Zelda Esc

---
+++ UnityZeldaEsc/Base/Assets/Scripts/Coins.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    public int coinsVal = 0;
+    Text score;
+
+    private void Start()
+    {
+        score = GetComponent<Text>();
+
+    }
+     void Update()
+    {
+        score.text = coinsVal.ToString();
+    }
+}
