87a634aef79e15e7703f0e8b9976ada394f5a9de
Syk-i <34940095+Syk-i@users.noreply.github.com>
2019-01-03 14:33:42 +0300

    KnockBack Working

--- UnityZeldaEsc/Base/Assets/Scripts/KnockBack.cs
+++ UnityZeldaEsc/Base/Assets/Scripts/KnockBack.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KnockBack : MonoBehaviour {
-
+    public float thrust;
+    public float knockTime;
 	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if(enemy != null)
+            {
+                enemy.isKinematic = false;
+                Vector2 difference = enemy.transform.position - transform.position;
+
+                difference = difference.normalized * thrust;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
+            }
+        }
+    }
+
+
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+            enemy.isKinematic = true;
+        }
+    }
 }
